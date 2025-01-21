3efcd3518020d26f7f4bbe712096badf7d4ab649
itg <itg@DESKTOP-KQ8SDGL>
2025-01-21 14:55:24 +0900

    update

--- LoginSystem/src/test/java/com/itg/examp/test/LoginSystemApplicationTests.java
+++ LoginSystem/src/test/java/com/itg/examp/test/LoginSystemApplicationTests.java
@@ -1,9 +1,11 @@
-package com.itgwangju.test;
+package com.itg.examp.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootTest
+@EnableScheduling
 class LoginSystemApplicationTests {
 
 	@Test

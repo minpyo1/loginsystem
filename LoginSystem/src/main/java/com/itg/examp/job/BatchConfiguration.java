1444bbbca1e71aae35f6a64530d1fa6d305cf686
itg <itg@DESKTOP-KQ8SDGL>
2025-01-24 15:37:31 +0900

    end

--- LoginSystem/src/main/java/com/itg/examp/job/BatchConfiguration.java
+++ LoginSystem/src/main/java/com/itg/examp/job/BatchConfiguration.java
@@ -13,26 +13,28 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BatchConfiguration {
-	int count=0;
-	@Bean
+	int count=0;// 배치 작업 수행 횟수를 기록하기 위한 변수
+	@Bean// Job 객체를 Spring 컨텍스트에 등록
 	public Job helloJob(JobRepository jobRepository,Step helloStep) {
+		 // JobBuilder를 사용해 Job 정의. 첫 번째 Step으로 helloStep을 실행
 		return new JobBuilder("helloJob",jobRepository)
-				.start(helloStep)
-				.build();
+				.start(helloStep)// helloStep을 첫 번째 Step으로 설정
+				.build();// Job 생성
 	}
-	@Bean
+	@Bean// Step 객체를 Spring 컨텍스트에 등록
 	public Step helloStep(JobRepository jobRepository,Tasklet hellolTasklet,PlatformTransactionManager transactionManager) {
+		// StepBuilder를 사용해 Step 정의. Tasklet과 트랜잭션 매니저를 설정
 		return new StepBuilder("helloStep",jobRepository)
-				.tasklet(hellolTasklet,transactionManager)
+				.tasklet(hellolTasklet,transactionManager)// Tasklet과 트랜잭션 설정
 				.allowStartIfComplete(true)// true 이면 배치작업 반복
-				.build();
+				.build();// Step 생성
 	}
 	@Bean
 	public Tasklet hellolTasklet() {//배치작업 수행
 		return (contribution,chunkContext)->{
-			count++;
+			count++;// 배치 작업 실행 횟수 증가
 			System.out.println(count+":: 스프링 배치작업 수행중...");
-			return RepeatStatus.FINISHED;			
+			return RepeatStatus.FINISHED;// 작업이 성공적으로 완료되었음을 나타냄	
 		};
 	}
 }
--- LoginSystem/src/main/java/com/itg/examp/job/BatchSchedule.java
+++ LoginSystem/src/main/java/com/itg/examp/job/BatchSchedule.java
@@ -14,18 +14,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class BatchSchedule {
 	@Autowired
-	private final Job job=null;
+	private final Job job=null;// Spring Batch의 Job 객체를 주입받아 사용
 	@Autowired
 	private final JobLauncher jobLauncher= null;
-	@Scheduled(cron="0 0/1 * * * ?")//스케쥴 실행 어노테이션
+	@Scheduled(cron="0 0/5 * * * ?")//스케쥴 실행 어노테이션
 	public void runJob() {
 		System.out.println("스케줄실행 ==== "); 
 		try {
+			// jobLauncher를 사용해 job 실행. 실행 시 JobParameters를 전달
 			jobLauncher.run(job, new JobParametersBuilder().toJobParameters());
 		} catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException
 				| JobParametersInvalidException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// 발생할 수 있는 예외를 처리 (예: 이미 실행 중인 Job, 재시작 불가능한 Job 등)
+			e.printStackTrace();// 예외 발생 시 스택 트레이스 출력
 		}
 		
 	}
